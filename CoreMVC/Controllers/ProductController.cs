7260aa4d097c959ce837f71031cac35a55e0427b
MikeWilkins <michael-wilkins@sky.com>
2021-06-17 13:15:00 +0100

    Added Product Index page

---
+++ CoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _db;
+
+        public ProductController(IProductRepository db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _db.GetAllProducts();
+            return View(productList);
+        }
+    }
+}
